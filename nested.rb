a5dd30623a195124a5ef386f5bd26769ca60439f
Nicolas Bermudez <bermnicolas@gmail.com>
2019-01-22 19:22:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
+programmer_hash[:grace_hopper]
 
 end
 
@@ -56,6 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+  programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
